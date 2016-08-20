09b6f563443ae53d2ea17571288412e86515136b
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-08-20 14:27:16 +0200

    added lockless queue classes

---
+++ include/bm/bm_sim/lockless_queueing.h
@@ -0,0 +1,318 @@
+/* Copyright 2016 University of Pisa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Yuri Iozzelli (y.iozzelli@gmail.com)
+ *
+ */
+
+//! @file lockless_queueing.h
+
+#ifndef BM_BM_SIM_LOCKLESS_QUEUEING_H_
+#define BM_BM_SIM_LOCKLESS_QUEUEING_H_
+
+#include <bm/bm_sim/spsc_queue.h>
+#include <bm/bm_sim/binary_semaphore.h>
+
+#include <deque>
+#include <ratio>
+#include <queue>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <algorithm>  // for std::max
+
+namespace bm {
+
+//! One of the most basic queueing block possible. Lets you choose (at runtime)
+//! the desired number of queues and the number of worker threads that
+//! will be reading from these queues. //!
+//!
+//! It is similar to the QueueingLogic class, but uses SPSCQueue internally.
+//! For this reason, at most one producer thread can push on a single queue,
+//! while different producers can push to different queues.
+//! The consumer threads must respect the mapping provided by `map_to_worker` 
+//! (see below).
+//!
+//! As of now, the behavior is blocking for both read (pop_back()) and write
+//! (push_front()).
+//!
+//! Template parameter `T` is the type (has to be movable) of the objects that
+//! will be stored in the queues. Template parameter `FMap` is a callable object
+//! that has to be able to map every logical queue id to a worker id. The
+//! following is a good example of functor that meets the requirements:
+//! @code
+//! struct WorkerMapper {
+//!   WorkerMapper(size_t nb_workers)
+//!       : nb_workers(nb_workers) { }
+//!
+//!   size_t operator()(size_t queue_id) const {
+//!     return queue_id % nb_workers;
+//!   }
+//!
+//!   size_t nb_workers;
+//! };
+//! @endcode
+template <typename T, typename FMap>
+class QueueingLogicLL {
+ public:
+  QueueingLogicLL(size_t nb_queues, size_t nb_workers, size_t capacity,
+                  FMap map_to_worker)
+      : nb_queues(nb_queues), nb_workers(nb_workers),
+        map_to_worker(map_to_worker), workers(nb_workers){
+    for (size_t i = 0; i < nb_queues; i++){
+      workers[map_to_worker(i)].add_queue(i);
+      queues.emplace_back(new SPSCQueue<T>(capacity,workers[map_to_worker(i)].sem));
+    }
+  }
+
+  //! Makes a copy of \p item and pushes it to the front of the logical queue
+  //! with id \p queue_id.
+  void push_front(size_t queue_id, const T &item) {
+    queues.at(queue_id)->push_front_nb(item);
+  }
+
+  //! Moves \p item to the front of the logical queue with id \p queue_id.
+  void push_front(size_t queue_id, T &&item) {
+    queues.at(queue_id)->push_front_nb(std::move(item));
+  }
+
+  void pop_back(size_t worker_id, size_t *queue_id, T *pItem) {
+    while(true) {
+      WorkerInfo& worker = workers[worker_id];
+      size_t n = worker.queues.size();
+      for (size_t i = 0; i < n; i++) {
+        size_t q = worker.next_queue();
+        if (queues[q]->pop_back_nb(pItem)) {
+          *queue_id = q;
+          worker.next_queue();
+          return;
+        }
+      }
+      std::this_thread::sleep_for(std::chrono::microseconds(1));
+      for (size_t i = 0; i < n; i++) {
+        size_t q = worker.next_queue();
+        if (queues[q]->pop_back_nb(pItem)) {
+          *queue_id = q;
+          worker.next_queue();
+          return;
+        }
+      }
+      for (size_t i = 0; i < n; i++) {
+        size_t q = worker.next_queue();
+        if (queues[q]->set_cons_event(1)) {
+          queues[q]->pop_back_nb(pItem);
+          *queue_id = q;
+          worker.next_queue();
+          return;
+        }
+      }
+      worker.sem->wait();
+    }
+  }
+
+  //! Deleted copy constructor
+  QueueingLogicLL(const QueueingLogicLL &) = delete;
+  //! Deleted copy assignment operator
+  QueueingLogicLL &operator =(const QueueingLogicLL &) = delete;
+
+  //! Deleted move constructor
+  QueueingLogicLL(QueueingLogicLL &&) = delete;
+  //! Deleted move assignment operator
+  QueueingLogicLL &&operator =(QueueingLogicLL &&) = delete;
+
+
+  //! Utility struct to keep track of the last checked queue. Needed to avoid 
+  //  starvation (by doing a round robin schedule)
+  struct WorkerInfo {
+    std::shared_ptr<BinarySemaphore> sem;
+    std::vector<size_t> queues;
+    size_t cur_idx;
+
+    WorkerInfo() {
+      sem = std::make_shared<BinarySemaphore>();
+      cur_idx = -1;
+    }
+
+    void add_queue(size_t q) {
+      queues.push_back(q);
+    }
+
+    size_t next_queue() {
+      cur_idx++;
+      if (cur_idx == queues.size()) {
+        cur_idx = 0;
+      }
+      return queues[cur_idx];
+    }
+  };
+
+ private:
+  size_t nb_queues;
+  size_t nb_workers;
+  FMap map_to_worker;
+ public:
+  std::vector<std::unique_ptr<SPSCQueue<T>>> queues;
+  std::vector<WorkerInfo> workers;
+};
+
+//! This class is slightly more advanced than QueueingLogicLL. The difference
+//! between the 2 is that this one offers the ability to rate-limit every
+//! logical queue, by providing a maximum number of elements consumed per
+//! second. If the rate is too small compared to the incoming packet rate, or if
+//! the worker thread cannot sustain the desired rate, elements are buffered in
+//! the queue. However, the write behavior (push_front()) for this class is
+//! different than the one for QueueingLogic. It is not blocking: if the queue
+//! is full, the function will return immediately and the element will not be
+//! queued. Look at the documentation for QueueingLogic for more information
+//! about the template parameters (they are the same).
+template <typename T, typename FMap>
+class QueueingLogicLLRL {
+ public:
+  using clock = std::chrono::high_resolution_clock;
+
+  QueueingLogicLLRL(size_t nb_queues, size_t nb_workers, size_t capacity,
+                  FMap map_to_worker)
+      : nb_queues(nb_queues), nb_workers(nb_workers),
+        map_to_worker(map_to_worker), workers(nb_workers){
+    for (size_t i = 0; i < nb_queues; i++){
+      workers[map_to_worker(i)].add_queue(i);
+      queues.emplace_back(new SPSCQueue<T>(capacity,workers[map_to_worker(i)].sem));
+    }
+  }
+
+  //! Makes a copy of \p item and pushes it to the front of the logical queue
+  //! with id \p queue_id.
+  void push_front(size_t queue_id, const T &item) {
+    queues.at(queue_id)->push_front_nb(item);
+  }
+
+  //! Moves \p item to the front of the logical queue with id \p queue_id.
+  void push_front(size_t queue_id, T &&item) {
+    queues.at(queue_id)->push_front_nb(std::move(item));
+  }
+
+  void pop_back(size_t worker_id, size_t *queue_id, T *pItem) {
+    WorkerInfo& worker = workers[worker_id];
+    size_t n = worker.queues.size();
+    while(true) {
+      clock::time_point next_time = clock::time_point::max();
+
+      for (size_t i = 0; i < n; i++) {
+        QueueInfo& q = worker.next_queue();
+        if (q.next_pop <= clock::now() && queues[q.index]->pop_back_nb(pItem)) {
+          *queue_id = q.index;
+          q.sent();
+          worker.next_queue();
+          return;
+        } else if (q.next_pop > clock::now()) {
+          next_time = std::min(next_time, q.next_pop);
+        }
+      }
+      for (size_t i = 0; i < n; i++) {
+        QueueInfo& q = worker.next_queue();
+        if (q.next_pop <= clock::now() && queues[q.index]->set_cons_event(1)) {
+          queues[q.index]->pop_back_nb(pItem);
+          *queue_id = q.index;
+          q.sent();
+          worker.next_queue();
+          return;
+        } else if (q.next_pop > clock::now()) {
+          next_time = std::min(next_time, q.next_pop);
+        }
+      }
+      worker.sem->wait_until(next_time);
+    }
+  }
+
+  void set_rate(size_t queue_id, uint64_t pps) {
+    workers[map_to_worker(queue_id)].queues[queue_id].pps = pps;
+  }
+
+  //! Deleted copy constructor
+  QueueingLogicLLRL(const QueueingLogicLLRL &) = delete;
+  //! Deleted copy assignment operator
+  QueueingLogicLLRL &operator =(const QueueingLogicLLRL &) = delete;
+
+  //! Deleted move constructor
+  QueueingLogicLLRL(QueueingLogicLLRL &&) = delete;
+  //! Deleted move assignment operator
+  QueueingLogicLLRL &&operator =(QueueingLogicLLRL &&) = delete;
+
+
+ private:
+  struct QueueInfo {
+    size_t index;
+    std::atomic<uint64_t> pps;
+    clock::time_point last_pop;
+    clock::time_point next_pop;
+    QueueInfo(size_t index): index(index), pps(0){
+      next_pop = clock::now();
+    }
+    QueueInfo(const QueueInfo& q): index(q.index){
+      pps = q.pps.load();
+      next_pop = clock::now();
+    }
+    void sent() {
+      if (pps == 0ul) {
+        return;
+      } else {
+        std::chrono::nanoseconds interval(1000000000 / pps);
+        next_pop += interval;
+      }
+    }
+    void set_rate(uint64_t pps) {
+      this->pps.store(pps);
+      next_pop = clock::now();
+    }
+  };
+
+  //! Utility struct to keep track of the last checked queue. Needed to avoid 
+  //  starvation (by doing a round robin schedule)
+  struct WorkerInfo {
+    std::shared_ptr<BinarySemaphore> sem;
+    std::vector<QueueInfo> queues;
+    size_t cur_idx;
+
+    WorkerInfo() {
+      sem = std::make_shared<BinarySemaphore>();
+      cur_idx = -1;
+    }
+
+    void add_queue(size_t q) {
+      queues.emplace_back(q);
+    }
+
+    QueueInfo& next_queue() {
+      cur_idx++;
+      if (cur_idx == queues.size()) {
+        cur_idx = 0;
+      }
+      return queues[cur_idx];
+    }
+
+  };
+
+  size_t nb_queues;
+  size_t nb_workers;
+  FMap map_to_worker;
+ public:
+  std::vector<std::unique_ptr<SPSCQueue<T>>> queues;
+  std::vector<WorkerInfo> workers;
+};
+}  // namespace bm
+
+#endif  // BM_BM_SIM_LOCKLESS_QUEUEING_H_
---
+++ include/bm/bm_sim/spsc_queue.h
@@ -0,0 +1,329 @@
+/* Copyright 2016 University of Pisa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Yuri Iozzelli (y.iozzelli@gmail.com)
+ *
+ */
+
+//! @file spsc_queue.h
+
+#ifndef BM_BM_SIM_SPSC_QUEUE_H_
+#define BM_BM_SIM_SPSC_QUEUE_H_
+
+#include <bm/bm_sim/binary_semaphore.h>
+
+#include <memory>
+#include <string>
+#include <iostream>
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <queue>
+
+#include <cmath>
+#include <cassert>
+
+namespace bm{
+
+/*
+ * This class implements a Single-producer Single-consumer lockless queue.
+ * Support for batch insertions is through the 'force' argument which,
+ * when set, exports the new queue index to the consumer.
+ * XXX we also export the info if the queue becomes full
+ * Removal can be of a single packet, or of an array.
+ *
+ * Indexes span the entire index_t range, so actual accesses need
+ * to use normalize_index() to reduce to ring_capacity (the physical
+ * number of entries). The logical size queue_capacity can be
+ * arbitrary, ring_capacity is the next power of 2 >= queue_capacity.
+ *
+ * The class uses atomic variables for the shared indexes between consumer and producer
+ * threads. In order to avoid unnecessary notifications on the semaphores, both
+ * the producer and the consumer set an event index corresponding to the point
+ * in the queue at which the other thread issues a notification.
+ * The consumer sets the event at the next producer index, while the producer
+ * can set a point further on in the ring.
+ */
+template <class T>
+class SPSCQueue {
+ public:
+  using index_t = uint64_t; // XXX could be uint32_t
+  using atomic_index_t = std::atomic<index_t>;
+  static constexpr size_t max_size = 1ul<<(sizeof(index_t)*8-1);
+
+  /*
+   * Multiple queues can share the same semaphore for synchronisation.
+   * By default, the consumer and producer semaphores (cs, ps) are
+   * null and the class creates private objects.
+   * Shared semaphores, if needed, should be passed as arguments
+   * to the constructor.
+   */
+  SPSCQueue(size_t max_capacity = 1024, 
+            std::shared_ptr<BinarySemaphore> cs=nullptr,
+            std::shared_ptr<BinarySemaphore> ps=nullptr)
+    : ring_capacity(1ul<<uint64_t(ceil(log2(max_capacity)))),
+      queue_capacity(max_capacity),
+      ring(new T[ring_capacity]),
+      cons_sem_ptr(cs),prod_sem_ptr(ps) {
+    assert(ring_capacity <= max_size);
+    if (cons_sem_ptr.get()==nullptr) {
+      cons_sem_ptr = std::make_shared<BinarySemaphore>();
+    }
+    if (prod_sem_ptr.get()==nullptr) {
+      prod_sem_ptr = std::make_shared<BinarySemaphore>();
+    }
+  }
+
+  // Blocking and non-blocking versions of insert functions,
+  // only for one item at a time.
+
+  //! Moves \p item to the front of the queue (producer)
+  bool push_front(T&& item, bool force = true) {
+    return push_front_forward(std::move(item), force);
+  }
+  //! Copies \p item to the front of the queue (producer)
+  bool push_front(const T& item, bool force = true) {
+    return push_front_forward(item, force);
+  }
+  //! Moves \p item to the front of the queue (producer). Non-blocking version
+  bool push_front_nb(T&& item, bool force = true) {
+    return push_front_forward_nb(std::move(item), force);
+  }
+  //! Copies \p item to the front of the queue (producer). Non-blocking version
+  bool push_front_nb(const T& item, bool force = true) {
+    return push_front_forward_nb(item, force);
+  }
+
+  // Blocking and non blocking versions of remove functions,
+  // for one or a vector of items.
+
+  //! Pops an element from the back of the queue: moves the element to `*pItem`.
+  // (consumer)
+  bool pop_back(T* pItem) {
+    cons_wait_data(1);
+    *pItem = std::move(ring[normalize_index(cons_ci)]);
+    cons_advance(1);
+
+    return true;
+  }
+  //! Pops an element from the back of the queue: moves the element to `*pItem`.
+  // (consumer)
+  bool pop_back(std::vector<T>* container) {
+    index_t num = cons_wait_data(1);
+    for (index_t i = 0; i < num; i++) {
+      container->push_back(std::move(ring[normalize_index(cons_ci+i)]));
+    }
+    cons_advance(num);
+
+    return true;
+  }
+
+  bool pop_back_nb(T* pItem) {
+    if (!cons_has_data(1)) {
+      return false;
+    }
+
+    *pItem = std::move(ring[normalize_index(cons_ci)]);
+    cons_advance(1);
+
+    return true;
+  }
+
+  //  Used by the consumer to set the cons_event.
+  //  Returns true if `wait` elements available, false otherwise
+  bool set_cons_event(index_t want) {
+    //request wake up when prod_index > cons_event
+    cons_event = cons_ci + want - 1;
+    cons_notify();
+    if (cons_has_data(want)) {
+      return true;
+    }
+    return false;
+  }
+
+  // copy and move constructors and assignment are not supported:
+  //! Deleted copy constructor
+  SPSCQueue(const SPSCQueue &) = delete;
+  //! Deleted copy assignment operator
+  SPSCQueue &operator =(const SPSCQueue &) = delete;
+
+  //! Deleted move constructor (class includes mutex)
+  SPSCQueue(SPSCQueue &&) = delete;
+  //! Deleted move assignment operator (class includes mutex)
+  SPSCQueue &&operator =(SPSCQueue &&) = delete;
+
+ private:
+  //! Moves/Copy \p item to the front of the queue (producer)
+  // This new template parameter U is necessary so the std::forward<U>()
+  // used below can automatically deduce if item is an rvalue reference or
+  // a const reference
+  template <typename U>
+  bool push_front_forward(U &&item, bool force) {
+    prod_wait_space(1);
+    // sure to have space
+    ring[normalize_index(prod_pi)] = std::forward<U>(item);
+    prod_advance(1, force);
+
+    return true;
+  }
+
+  //! Moves/Copy \p item to the front of the queue (producer)
+  // This new template parameter U is necessary so the std::forward<U>()
+  // used below can automatically deduce if item is an rvalue reference or
+  // a const reference. Non-blocking version
+  template <typename U>
+  bool push_front_forward_nb(U &&item, bool force) {
+    if (!prod_has_space(1)) return false;
+    // sure to have space
+    ring[normalize_index(prod_pi)] = std::forward<U>(item);
+    prod_advance(1, force);
+
+    return true;
+  }
+
+
+  //! Used by the consumer to wait for 'want' elements.
+  //  Returns number of available elements
+  index_t cons_wait_data(index_t want) {
+    while (true) {
+      if (cons_has_data(want)) break;
+      // sleep a while and retry
+      std::this_thread::sleep_for(std::chrono::microseconds(cons_sleep_time));
+      if (cons_has_data(want)) break;
+
+      if (set_cons_event(want)) break;
+
+      cons_sem_ptr->wait(); // wait for notification
+    }
+    return cons_pi - cons_ci;
+  }
+
+  // used by consumer to update shared consumer index and signal the producer
+  // if producer event is reached
+  void cons_notify() {
+    index_t old = __cons_index;
+    // update shared consumer index
+    __cons_index = cons_ci;
+
+    index_t pe = prod_event;
+
+    if (index_t(cons_ci - pe - 1) < index_t(cons_ci - old)) {
+      prod_sem_ptr->signal();
+      prod_notified++;
+    }
+  }
+
+  // used by consumer to advance its index and check if a notification is needed
+  void cons_advance(index_t have) {
+    cons_ci += have;
+    if(cons_pi == cons_ci) {
+      cons_notify();
+    }
+  }
+
+  // used by the producer to wait until 'want' slots are available to fill
+  // returns the number of available slots
+  index_t prod_wait_space(index_t want) { // returns available space
+    while (true) {
+      if (prod_has_space(want)) {
+        break;
+      }
+      // wake up when prod_c moves past prod_event (75% of current size)
+      prod_event = index_t(prod_ci + index_t(prod_pi-prod_ci)/4);
+      prod_notify();
+      if (prod_has_space(want)) { // double check
+        break;
+      }
+      // not enough space
+      prod_sem_ptr->wait();
+      prod_ci = __cons_index;
+    }
+    return prod_ci + queue_capacity - prod_pi;
+  }
+
+  // used by producer to update shared producer index and signal the consumer
+  // if consumer event is reached
+  void prod_notify() {
+    index_t old = __prod_index;
+    __prod_index = prod_pi;
+
+    index_t ce = cons_event;
+
+    if (index_t(prod_pi - ce - 1) < index_t(prod_pi - old)) {
+      cons_sem_ptr->signal();
+      cons_notified++;
+    }
+  }
+
+  // used by consumer to advance its index
+  // update shared state and notify only if 'force' is true
+  void prod_advance(index_t have, bool force) {
+    prod_pi+=have;
+    if (force) {
+      prod_notify();
+    }
+  }
+
+  // maps the index position in the ring to the actual array index
+  size_t normalize_index(index_t index) {
+    return index & (ring_capacity-1);
+  }
+
+  // check if 'want' slots are available for the producer
+  bool prod_has_space(index_t want) {
+    prod_ci = __cons_index;
+    return (index_t(prod_pi-prod_ci) <= queue_capacity - want);
+  }
+
+  // check if 'want' elements are available for the consumer
+  bool cons_has_data(index_t want) {
+    cons_pi = __prod_index;
+    return (index_t(cons_pi - cons_ci) >= want);
+  }
+
+private:
+  static constexpr int cons_sleep_time{1}; // microseconds
+
+  alignas(64)
+  const size_t ring_capacity;
+  const size_t queue_capacity;
+  const std::unique_ptr<T[]> ring;
+  alignas(64)
+  std::shared_ptr<BinarySemaphore> cons_sem_ptr;
+  std::shared_ptr<BinarySemaphore> prod_sem_ptr;
+
+  alignas(64)
+  atomic_index_t __prod_index{0}; // index of the next element to produce
+  atomic_index_t prod_event{0}; // wake up when cons_index > prod_event
+  index_t prod_ci{0}; // copy of consumer index (used by producer)
+  index_t prod_pi{0}; // copy of producer index (used by producer)
+
+  alignas(64)
+  atomic_index_t __cons_index{0}; // index of the next element to consume
+  atomic_index_t cons_event{0}; // wake up when prod_index > cons_event
+  index_t cons_ci{0}; // copy of consumer index (used by consumer)
+  index_t cons_pi{0}; // copy of producer index (used by consumer)
+
+ public:
+  alignas(64)
+  uint64_t prod_notified{0};
+  alignas(64)
+  uint64_t cons_notified{0};
+};
+
+} // namespace bm
+
+#endif // BM_BM_SIM_SPSC_QUEUE_H_
